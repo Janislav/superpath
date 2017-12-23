7af2fbed28e381e846c96798c3861a7adb63e236
Janislav <jan.boerner@nexiles.com>
2017-12-23 13:34:09 +0100

    add readme and fixed bug which ends up in not dissolving stuff for unix

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
  */
 const path = require('path');
 
+let replaceAll = function(target ,search, replacement) {
+    return target.split(search).join(replacement);
+};
+
 /**
  * SuperPath:
  *
@@ -25,39 +29,39 @@ class SuperPath {
     }
 
     /**
-     * Resolves a path from the current process.env state.
+     * Dissolves a path from the current process.env state.
      *
      * @param {String} p path as a string
      * @returns {*}
      */
-    resolve(p) {
+    dissolve(p) {
         let env = process.env;
-        return this.resoleFromEnvironemnt (p, env);
+        return this.dissolveFromEnvironment (p, env);
     }
 
     /**
-     * Resolves a path from a given environment object.
+     * Dissolves a path from a given environment object.
      *
      * @param {String} p the path as a string
      * @param {Object} e the env as key value object
      * @returns {XML|string|XMLList}
      */
-    resolveFromEnvironemnt(p, e) {
+    dissolveFromEnvironment (p, e) {
         let pp = path.normalize(p);
 
-        for(let key in e){
+        for(let key in e) {
             let value = e[key];
 
             let envvar, regex;
 
-            if (this.os=="win32") {
+            if (this.os==="win32") {
                 envvar = `%${key}%`;
+                regex = new RegExp(envvar, 'g');
+                pp = pp.replace(regex, value);
             } else {
                 envvar = `$${key}`;
+                pp = replaceAll(pp, envvar, value);
             }
-
-            regex = new RegExp(envvar, 'g');
-            pp = pp.replace(regex, value);
         }
 
         pp = path.normalize(pp);
